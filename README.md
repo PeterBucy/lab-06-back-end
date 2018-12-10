45d0437311a6dd789e9cf8680889530e9d575f87
erikmjohnson <erikarisma98@gmail.com>
2018-12-10 14:31:51 -0800

    Complete

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## Time Estimates
 
-* Task 1: Additiional Functionality
+* Task 1: Obtaining Longitude and Latitude
   * Estimated Time of Completion: __60 hours__
   * Start Time: __18:30__
   * End Time: __20:30__
   * Actual Time of Completion: __2 hours__
+
+* Task 2: Retrieving Weather Information
+  * Estimated Time of Completion: __60 hours__
+  * Start Time: __18:30__
+  * End Time: __20:30__
+  * Actual Time of Completion: __2 hours__
+
+* Task 3: Additiional Functionality
+  * Not Required
+
--- server.js
+++ server.js
@@ -20,8 +20,8 @@ function Location(location){
 }
 
 function Weather(weather) {
-  this.forcast = weather.summary;
-  this.time = weather.time;
+  this.forecast = weather.summary;
+  this.time = new Date(weather.time * 1000).toDateString();
 }
 
 // Router
@@ -31,12 +31,15 @@ app.get('/weather', getWeather)
 
 //Handlers
 function getWeather (request, response) {
-  const weatherData = searchForWeather(request.query)
+  const weatherData = searchForWeather(request.query.data)
   response.send(weatherData);
 }
 
 function getLocation (req, res) {
   const locationData = searchToLatLong(req.query.data); // 'Lynnwood, WA'
+  if(!locationData) {
+    error(res);
+  }
   res.send(locationData);
 }
 
@@ -46,11 +49,15 @@ function searchForWeather (query) {
   weatherData.daily.data.forEach(forecast => dailyArray.push(new Weather(forecast)));
   return dailyArray;
 }
-
 function searchToLatLong(query){
   const geoData = require('./data/geo.json');
   const location = new Location(geoData.results[0]);
   return location;
+}
+
+//Error Handler
+function error (res) {
+  res.send(500).send('Sorry! Not found!!!');
 }
 
 // Give error messages if incorrect
@@ -58,10 +65,6 @@ function searchToLatLong(query){
 app.get('/*', function(req, res) {
   res.status(404).send('Success!');
 })
-
-// app.get('/*', function (req, res) {
-//   res.status(500).send('Sorry! Something went horribly wrong!');
-// })
 
 app.listen(PORT, () => {
   console.log(`app is up on port : ${PORT}`)
